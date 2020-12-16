c18940ba46e28e445af4cb8fd996a71f6a5ed7e0
PiotrWR <pwrynio@gmail.com>
2020-12-16 18:31:13 +0100

    Not working commit

--- traditional/spec/dose-controller.spec.js
+++ traditional/spec/dose-controller.spec.js
@@ -1,63 +1,91 @@
-const MedicinePump = require('../dependencies/medicine-pump')
-const HealthMonitor = require('../dependencies/health-monitor')
-const AlertService = require('../dependencies/alert-service')
-const DoseController = require('../src/dose-controller')
+const MedicinePump = require("../dependencies/medicine-pump");
+const HealthMonitor = require("../dependencies/health-monitor");
+const AlertService = require("../dependencies/alert-service");
+const DoseController = require("../src/dose-controller");
 
-
-describe('Dose Controller', function () {
-
+describe("Dose Controller", function () {
   let medicinePump;
   let doseController;
 
-  beforeEach(function () {
-  })
+  beforeEach(function () {});
 
-  function given ({ pressure }) {
+  function given({ pressure, medicine = "LowerPressure" }) {
     medicinePump = {
       dose: jest.fn(), //funckja, której wywołania będziemy sledzić
-      getTimeSinceLastDoseInMinutes: function (medicine) {
-        // we don't care what the implementation of this method is
-      }
-    }
+      getTimeSinceLastDoseInMinutes: jest.fn(medicine).mockReturnValueOnce(10).mockReturnValueOnce(1),
+    };
+    // getTimeSinceLastDoseInMinutes: jest.fn(medicine).mockReturnValueOnce(10).mockReturnValueOnce(1),
     const healthMonitor = {
       getSystolicBloodPressure: function () {
-        return pressure
-      }
-    }
-    const alertService = AlertService()
+        return pressure;
+      },
+    };
+    const alertService = AlertService();
 
-    doseController = DoseController(healthMonitor, medicinePump, alertService)
+    doseController = DoseController(healthMonitor, medicinePump, alertService);
   }
 
-  it('Gdy ciśnienie spadnie poniżej 90, podaj 1 dawkę leku podnoszącego ciśnienie.', () => {
+  it("Gdy ciśnienie spadnie poniżej 90, podaj 1 dawkę leku podnoszącego ciśnienie.", () => {
     //given
-    given({ pressure: 89 })
+    given({
+      pressure: 89,
+    });
 
     //when
-    doseController.checkHealthAndApplyMedicine()
+    doseController.checkHealthAndApplyMedicine();
 
     //then
-    dosedMedicine({ name: "RaisePressure", count: 1 })
-  })
+    dosedMedicine({
+      name: "RaisePressure",
+      count: 1,
+    });
+  });
 
-  it('Gdy ciśnienie spadnie poniżej 60, podaj 2 dawki leku podnoszącego ciśnienie.', () => {
-    given({ pressure: 59 })
+  it("Gdy ciśnienie spadnie poniżej 60, podaj 2 dawki leku podnoszącego ciśnienie.", () => {
+    given({
+      pressure: 59,
+    });
 
-    doseController.checkHealthAndApplyMedicine()
+    doseController.checkHealthAndApplyMedicine();
 
-    dosedMedicine({ name: "RaisePressure", count: 2 })
-  })
+    dosedMedicine({
+      name: "RaisePressure",
+      count: 2,
+    });
+  });
 
-  it('Gdy ciśnienie wzrośnie powyżej 150, podaj lek obniżający ciśnienie.', () => {
-    given({ pressure: 160 })
+  it("Gdy ciśnienie wzrośnie powyżej 150, podaj lek obniżający ciśnienie.", () => {
+    given({
+      pressure: 160,
+    });
 
-    doseController.checkHealthAndApplyMedicine()
+    doseController.checkHealthAndApplyMedicine();
 
-    dosedMedicine({ name: "LowerPressure", count: 1 })
-  })
+    dosedMedicine({
+      name: "LowerPressure",
+      count: 1,
+    });
+  });
 
-  function dosedMedicine({name, count}){
-    expect(medicinePump.dose).toBeCalledWith({ name, count })
+  it("Gdy pompa nie zadziała 1 raz, ponów próbę.", () => {
+    given({
+      pressure: 160,
+      medicine: "LowerPressure",
+    });
+
+    doseController.checkHealthAndApplyMedicine();
+
+    dosedMedicine({
+      name: "LowerPressure",
+      count: 1,
+    });
+    expect(medicinePump.getTimeSinceLastDoseInMinutes).toBeCalledWith(1);
+  });
+
+  function dosedMedicine({ name, count }) {
+    expect(medicinePump.dose).toBeCalledWith({
+      name,
+      count,
+    });
   }
-
-})
+});
--- traditional/src/dose-controller.js
+++ traditional/src/dose-controller.js
@@ -1,18 +1,35 @@
-function DoseController (healthMonitor, medicinePump, alertService) {
-
+function DoseController(healthMonitor, medicinePump, alertService) {
+  // ! wartość ciśnienia / funkcja do podania leku / powiadomienie lekarza
   return {
-    checkHealthAndApplyMedicine: checkHealthAndApplyMedicine
+    checkHealthAndApplyMedicine: checkHealthAndApplyMedicine,
+    // checkIsGivenDosedMedicine: checkIsGivenDosedMedicine,
+  };
+
+  function checkHealthAndApplyMedicine() {
+    const pressure = healthMonitor.getSystolicBloodPressure();
+    if (pressure < 60) {
+      medicinePump.dose({
+        name: "RaisePressure",
+        count: 2,
+      });
+    } else if (pressure > 150) {
+      medicinePump.dose({
+        name: "LowerPressure",
+        count: 1,
+      });
+    } else {
+      medicinePump.dose({
+        name: "RaisePressure",
+        count: 1,
+      });
+    }
+    checkIsGivenDosedMedicine();
   }
 
-  function checkHealthAndApplyMedicine () {
-    const pressure = healthMonitor.getSystolicBloodPressure()
-    if (pressure < 60) {
-      medicinePump.dose({ name: "RaisePressure", count: 2 })
-    } else {
-      medicinePump.dose({ name: "RaisePressure", count: 1 })
+  function checkIsGivenDosedMedicine() {
+    if (medicinePump.getTimeSinceLastDoseInMinutes("LowerPressure") == 1) {
+      checkHealthAndApplyMedicine();
     }
   }
-
 }
-
-module.exports = DoseController
+module.exports = DoseController;
