c809c4d72c58da0ae4b705e9062ee3a791eb52d2
PiotrWR <pwrynio@gmail.com>
2020-12-28 11:04:16 +0100

    Add "not give drug if time is too short"

--- traditional/src/dose-controller.js
+++ traditional/src/dose-controller.js
@@ -7,22 +7,24 @@ function DoseController(healthMonitor, medicinePump, alertService) {
 
   function checkHealthAndApplyMedicine() {
     try {
-      const pressure = healthMonitor.getSystolicBloodPressure();
-      if (pressure < 60) {
-        medicinePump.dose({
-          name: "RaisePressure",
-          count: 2,
-        });
-      } else if (pressure > 150) {
-        medicinePump.dose({
-          name: "LowerPressure",
-          count: 1,
-        });
-      } else {
-        medicinePump.dose({
-          name: "RaisePressure",
-          count: 1,
-        });
+      if (medicinePump.getTimeSinceLastDoseInMinutes() > 30) {
+        const pressure = healthMonitor.getSystolicBloodPressure();
+        if (pressure < 60) {
+          medicinePump.dose({
+            name: "RaisePressure",
+            count: 2,
+          });
+        } else if (pressure > 150) {
+          medicinePump.dose({
+            name: "LowerPressure",
+            count: 1,
+          });
+        } else {
+          medicinePump.dose({
+            name: "RaisePressure",
+            count: 1,
+          });
+        }
       }
     } catch (e) {
       // checkIsGivenDosedMedicine();
